268e71fd5cb63451c8a8c1265611a4fc90757d31
LastWhisperXX <49542507+LastWhisperXX@users.noreply.github.com>
2020-08-11 23:55:50 +0800

    LastWhisperXX
    双重检查锁模式更新

--- fool/src/main/java/com/designpattern/creating/singleton/DoubleCheck.java
+++ fool/src/main/java/com/designpattern/creating/singleton/DoubleCheck.java
@@ -1,13 +1,29 @@
 package com.designpattern.creating.singleton;
 
 /**
- * @Describe 双重检查锁模式
+ * @Describe
+ * 双重检查锁模式
+ *  优点
+ *   线程安全(volatile阻止指令重拍 , synchronized线程锁)
+ *   保证效率
+ *  缺点
+ *   相对效率低
  * @ClassName DoubleCheck
  * @Author ZhuXunComputer
  * @Date 2020/8/4
  * @Version JDK1.8
  */
 public class DoubleCheck {
+    /**
+     * volatile关键字阻止指令重排
+     * 类初始化流程
+     * 1.分配对象内存空间
+     *  memory = allocate();
+     * 2.初始化对象
+     *  ctorInstance(memory);
+     * 3.讲对象指向分配的内存空间
+     *  instance =memory;
+     */
     private static volatile DoubleCheck doubleCheck ;
 
     private DoubleCheck(){};
---
+++ fool/src/main/java/com/designpattern/creating/singleton/StaticInside.java
@@ -0,0 +1,24 @@
+package com.designpattern.creating.singleton;
+
+/**
+ * @Descripton
+ * 静态内部类
+ *  优点
+ *  缺点
+ * @Classname StaticInside
+ * @Author CaoYang
+ * @Address 开发大道
+ * @Date 2020/8/11 23:47
+ * @Version 1.8.0
+ * @Createder by 寒酥
+ */
+public class StaticInside {
+    private static class Inside{
+        private static final StaticInside INSIDE = new StaticInside();
+    }
+    private StaticInside(){}
+
+    public static StaticInside getInstance(){
+        return Inside.INSIDE;
+    }
+}
