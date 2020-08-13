3dd9ece26d73bcf44b6f7449c21c575ca885acd8
LastWhisperXX <49542507+LastWhisperXX@users.noreply.github.com>
2020-08-13 23:17:11 +0800

    单例模式-饿汉式特性补充

--- fool/src/main/java/com/designpattern/creating/singleton/Hungry.java
+++ fool/src/main/java/com/designpattern/creating/singleton/Hungry.java
@@ -3,10 +3,12 @@ package com.designpattern.creating.singleton;
 /**
  * @Describe
  * 单例模式——饿汉式
+ *  不支持LazyLoad
  *  优点
  *   线程安全
  *  缺点
  *   浪费内存
+ *   类加载时就初始化
  * @ClassName Hungry
  * @Author ZhuXunComputer
  * @Date 2020/7/28
